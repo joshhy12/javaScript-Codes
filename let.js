a573f23e4340657a552cb03e6da67f65a70b1229
joshy Lucas <96329740+joshhy12@users.noreply.github.com>
2024-05-27 15:03:25 +0300

    "Added HTML boilerplate to index.html and modified let.js to change variable declaration from var to let and update demo text"

--- let.js
+++ let.js
@@ -12,20 +12,20 @@ Variables declared with let cannot be Redeclared in the same scope
 <html>
 <body>
 
-<h2>Redeclaring a Variable Using var</h2>
+<h2>Redeclaring a Variable Using let</h2>
 
 <p id="demo"></p>
 
 <script>
-var x = 10;
+let x = 10;
 // Here x is 10
 
 {  
-var x = 2;
-// Here x is 2
+let y = 2; 
+// Here y is 2
 }
 
-// Here x is 2
+// Here x is 10
 document.getElementById("demo").innerHTML = x;
 </script>
 
